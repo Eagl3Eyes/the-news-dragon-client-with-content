0db294355e9f88365515191cfecc7afc9c91b78b
Eagl3Eyes <58110053+Eagl3Eyes@users.noreply.github.com>
2023-04-23 23:36:42 +0600

    category news added

--- src/Shared/RightNav/RightNav.jsx
+++ src/Shared/RightNav/RightNav.jsx
@@ -8,7 +8,7 @@ const RightNav = () => {
     return (
         <div>
             <h4 className='mt-4'>Login With</h4>
-            <Button className='mb-2' variant="outline-primary"> <FaGoogle /> Login with Google</Button>
+            <Button className='mb-2' variant="outline-secondary"> <FaGoogle /> Login with Google</Button>
             <Button variant="outline-secondary"> <FaGithub /> Login with Github</Button>
 
             <div>
